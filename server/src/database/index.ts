e1f3772df478dd9231b9cf8467611677f9411a8c
Abhishek Jain <abhishekjai35@gmail.com>
2021-01-06 16:01:05 +0530

    Update Server dependencies

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -4,7 +4,10 @@ import { Database } from "../lib/types";
 const url = `${process.env.MONGODB_CONNECTION_STRING}`;
 
 export const connectDatabase = async (): Promise<Database> => {
-  const client = await MongoClient.connect(url, { useNewUrlParser: true });
+  const client = await MongoClient.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   const db = client.db("tinyhouse");
 
   return {
