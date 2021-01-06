ecd0dea793fc05bc0ef311aa0c6d6fcef9380ff8
Abhishek Jain <abhishekjai35@gmail.com>
2021-01-06 16:48:32 +0530

    Added Schema for the collections

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,19 +1,60 @@
 import { Collection, ObjectId } from "mongodb";
 
+export enum ListingType {
+  Apartment = "APARTMENT",
+  House = "HOUSE",
+}
+
+export interface BookingIndexMonth {
+  [key: string]: boolean;
+}
+
+export interface BookingsIndexYear {
+  [key: string]: BookingIndexMonth;
+}
+
+export interface BookingsIndex{
+  [key: string]: BookingsIndexYear
+}
 export interface Listing {
   _id: ObjectId;
+  title: string;
+  description: string;
+  image: string;
+  host: string;
+  type: ListingType;
+  address: string;
+  country: string;
+  admin: string;
+  city: string;
+  bookings: ObjectId[];
+  bookingsIndex: BookingsIndex;
+  price: number;
+  numOfGuests: number;
 }
 
 export interface User {
-  _id: ObjectId;
+  _id: string;
+  token: string;
+  name: string;
+  avatar: string;
+  contact: string;
+  walletId?: string;
+  income: number;
+  bookings: ObjectId[];
+  listings: ObjectId[];
 }
 
 export interface Bookings {
   _id: ObjectId;
+  listing: ObjectId;
+  tenant: string;
+  checkIn: string;
+  checkOut: string;
 }
 
 export interface Database {
-  bookings: Collection<Bookings>; 
+  bookings: Collection<Bookings>;
   listings: Collection<Listing>;
   users: Collection<User>;
 }
