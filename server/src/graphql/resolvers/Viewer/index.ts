cf9b61db4263a496d39341beb2dbbcc2bfb8c0d9
Abhishek Jain <abhishekjai35@gmail.com>
2021-01-08 21:15:30 +0530

    Added feature to logIn user from graphql api

--- server/src/graphql/resolvers/Viewer/index.ts
+++ server/src/graphql/resolvers/Viewer/index.ts
@@ -1,8 +1,74 @@
 import { IResolvers } from "apollo-server-express";
 import { Google } from "../../../lib/api";
-import { Viewer } from "../../../lib/types";
+import { Viewer, Database, User } from "../../../lib/types";
 import { LogInArgs } from "./types";
 import crypto from "crypto";
+
+const logInViaGoogle = async (
+  code: string,
+  token: string,
+  db: Database
+): Promise<User | undefined> => {
+  const { user } = Google.logIn(code);
+  if (!user) {
+    throw new Error("Google Login Error");
+  }
+
+  const userNamesList = user.names && user.names.length ? user.names : null;
+  const userPhotosList = user.photos && user.photos.length ? user.photos : null;
+  const userEmailsList =
+    user.emailAddresses && user.emailAddresses.length
+      ? user.emailAddresses
+      : null;
+
+  const userName = userNamesList ? userNamesList[0].displayName : null;
+  const userId =
+    userNamesList &&
+    userNamesList[0].metadata &&
+    userNamesList[0].metadata.source
+      ? userNamesList[0].metadata.source.id
+      : null;
+
+  const userAvatar =
+    userPhotosList && userPhotosList[0].url ? userPhotosList[0].url : null;
+
+  const userEmail =
+    userEmailsList && userEmailsList[0].value ? userEmailsList[0].value : null;
+
+  if (!userId || !userName || !userAvatar || !userEmail) {
+    throw new Error("Google login error");
+  }
+  const updateRes = await db.users.findOneAndUpdate(
+    { _id: userId },
+    {
+      $set: {
+        name: userName,
+        avatar: userAvatar,
+        contact: userEmail,
+        token,
+      },
+    },
+    { returnOriginal: false }
+  );
+
+  let viewer = updateRes.value;
+
+  if (!viewer) {
+    const inserRes = await db.users.insertOne({
+      _id: userId,
+      name: userName,
+      avatar: userAvatar,
+      contact: userEmail,
+      token,
+      income: 0,
+      bookings: [],
+      listings: [],
+    });
+    viewer = inserRes.ops[0];
+  }
+
+  return viewer;
+};
 
 export const viewerResolvers: IResolvers = {
   Query: {
@@ -15,11 +81,31 @@ export const viewerResolvers: IResolvers = {
     },
   },
   Mutation: {
-    logIn: (_root: undefined, { input }: LogInArgs) => {
+    logIn: async (
+      _root: undefined,
+      { input }: LogInArgs,
+      { db }: { db: Database }
+    ): Promise<Viewer> => {
       try {
         const code = input ? input.code : null;
         const token = crypto.randomBytes(16).toString("hex");
-      } catch (err) {}
+        const viewer: User | undefined = code
+          ? await logInViaGoogle(code, token, db)
+          : undefined;
+
+        if (!viewer) {
+          return { didRequest: true };
+        }
+        return {
+          _id: viewer._id,
+          token: viewer.token,
+          walletId: viewer.walletId,
+          avatar: viewer.avatar,
+          didRequest: true,
+        };
+      } catch (err) {
+        throw new Error(`Failed to login: ${err}`);
+      }
     },
     logOut: () => {
       return "Query.logOut";
